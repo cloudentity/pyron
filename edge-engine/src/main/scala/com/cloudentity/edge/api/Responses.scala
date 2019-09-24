197b6217a69f1f1bff97e9df79844c25a162c6f7
Marcin Nikliborc <mnikliborc@cloudentity.com>
2019-09-24 14:41:26 +0200

    cleaning API error responses

--- edge-engine/src/main/scala/com/cloudentity/edge/api/Responses.scala
+++ edge-engine/src/main/scala/com/cloudentity/edge/api/Responses.scala
@@ -18,22 +18,11 @@ object Responses {
     body.asJson(errorBodyEncoder).toString
 
   object Errors {
-    val invalidRequest                = Error(400, ErrorBody("Request.Invalid", "The request could not be understood by the server due to malformed syntax."))
-    val requiredIdentifierNotFound    = Error(400, ErrorBody("REQUIRED_IDENTIFIER_NOT_FOUND","Some of required identifiers could not be found"))
     val unauthenticated               = Error(401, ErrorBody("Authentication.Unauthenticated", "Not authenticated. This API is only available for authenticated users."))
-    val hmacMismatch                  = Error(401, ErrorBody("Authentication.Mismatch", "Provided hmac request does not match the original request."))
-    val hmacRequestOutdated           = Error(401, ErrorBody("Authentication.RequestOutdated", "The request was outdated."))
-    val ruleNotFound                  = Error(404, ErrorBody("NO_RULE","No matching rule found"))
-    val invalidCSRFHeader             = Error(403, ErrorBody("INVALID_CSRF", "CSRF header is invalid"))
-    val targetUnreachable             = Error(500, ErrorBody("TARGET_SERVICE_UNREACHABLE", "Could not call target service"))
+    val ruleNotFound                  = Error(404, ErrorBody("Rule.Missing","No matching rule found"))
+    val targetUnreachable             = Error(500, ErrorBody("Target.Unreachable", "Could not call target service"))
     val responseTimeout               = Error(504, ErrorBody("Response.Timeout", "Call to target service timed-out"))
     val systemTimeout                 = Error(504, ErrorBody("System.Timeout", "Request processing timed-out"))
-    val authzUnreachable              = Error(500, ErrorBody("AUTHZ_SERVICE_UNREACHABLE", "Could not call Authz service"))
-    val authzUnexpected               = Error(500, ErrorBody("AUTHZ_SERVICE_UNEXPECTED_CODE", "Authz service returned unexpected HTTP status code"))
-    val unexpected                    = Error(500, ErrorBody("UNEXPECTED_ERROR","Sorry, unexpected error occurred"))
-
-    def invalidRequest(msg: String): Error = {
-      Error(400, ErrorBody("Request.Invalid", msg))
-    }
+    val unexpected                    = Error(500, ErrorBody("System.Unexpected","Sorry, unexpected error occurred"))
   }
 }
