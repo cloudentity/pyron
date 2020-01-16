8e53341ca3ebb2d7fcd0a92addc9b90a04beaf63
Marcin Nikliborc <mnikliborc@cloudentity.com>
2020-01-16 11:47:10 +0100

    compile error fix

--- pyron-plugin-impl/src/main/scala/com/cloudentity/pyron/plugin/impl/cors/CorsPlugin.scala
+++ pyron-plugin-impl/src/main/scala/com/cloudentity/pyron/plugin/impl/cors/CorsPlugin.scala
@@ -122,7 +122,7 @@ class CorsPlugin extends RequestResponsePluginVerticle[CorsPluginConf] with Conf
       criteria = ruleConf.rule.criteria.copy(method = HttpMethod.OPTIONS)
     )
 
-    val preRequestPlugin = ApiGroupPluginConf(PluginName("cors"), confEncoder(conf))
+    val preRequestPlugin = ApiGroupPluginConf(PluginName("cors"), confEncoder(conf), None)
 
     ExtendRules(append = List(RuleConfWithPlugins(
       rule = ruleConfWithOptions,
