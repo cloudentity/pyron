d87f6b56a7a93c3cb491355ce9bef9adda6d8222
Aleksander Swirski <aswirski@cloudentity.com>
2021-04-07 14:40:48 +0200

    Replace deprecated ServiceClientFactory with VertxEndopointClient

--- pyron-core/src/main/scala/com/cloudentity/pyron/domain/http/RelativeUri.scala
+++ pyron-core/src/main/scala/com/cloudentity/pyron/domain/http/RelativeUri.scala
@@ -1,22 +1,10 @@
 package com.cloudentity.pyron.domain.http
 
-import cats.data.Kleisli
 import com.cloudentity.pyron.domain.flow.{PathParams, RewritePath}
 import com.cloudentity.pyron.rule.RewriteUtil.rewritePathWithPathParams
 
 import java.net.URI
 import scala.util.Try
-
-object RelativeUri {
-  def of(uriString: String): Try[RelativeUri] = Try(new URI(uriString)).flatMap { uri =>
-    val path = Option(uri.getPath).getOrElse("")
-    Option(uri.getQuery).fold(Try(QueryParams.of()))(QueryParams.fromString)
-      .map(query => FixedRelativeUri(UriPath(path), query, PathParams(Map())))
-  }
-
-  def fromPath(path: String): RelativeUri =
-    FixedRelativeUri(UriPath(path), QueryParams.of(), PathParams(Map()))
-}
 
 case class FixedRelativeUri(_path: UriPath, query: QueryParams, pathParams: PathParams) extends RelativeUri {
   def updateQuery(queryParams: QueryParams): FixedRelativeUri = this.copy(query = queryParams)
@@ -28,6 +16,17 @@ case class RewritableRelativeUri(_path: RewritePath, query: QueryParams, pathPar
   def updateQuery(queryParams: QueryParams): RewritableRelativeUri = this.copy(query = queryParams)
 
   def updatePathParams(pathParams: PathParams): RewritableRelativeUri = this.copy(pathParams = pathParams)
+}
+
+object RelativeUri {
+  def of(uriString: String): Try[RelativeUri] = Try(new URI(uriString)).flatMap { uri =>
+    val path = Option(uri.getPath).getOrElse("")
+    Option(uri.getQuery).fold(Try(QueryParams.of()))(QueryParams.fromString)
+      .map(query => FixedRelativeUri(UriPath(path), query, PathParams(Map())))
+  }
+
+  def fromPath(path: String): RelativeUri =
+    FixedRelativeUri(UriPath(path), QueryParams.of(), PathParams(Map()))
 }
 
 sealed trait RelativeUri {
