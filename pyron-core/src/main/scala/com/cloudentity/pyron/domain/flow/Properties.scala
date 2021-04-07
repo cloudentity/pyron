20f82153aa0ef5875289fbb5ddcd8931e34c5314
Aleksander Swirski <aswirski@cloudentity.com>
2021-04-07 03:40:58 +0200

    Split larger classes from flow.scala into their own files

---
+++ pyron-core/src/main/scala/com/cloudentity/pyron/domain/flow/Properties.scala
@@ -0,0 +1,17 @@
+package com.cloudentity.pyron.domain.flow
+
+import scala.util.Try
+
+case class Properties(private val ps: Map[String, Any]) {
+  def toMap(): Map[String, Any] = ps
+
+  def updated(key: String, value: Any): Properties =
+    Properties(ps.updated(key, value))
+
+  def get[A](key: String): Option[A] =
+    ps.get(key).flatMap { value => Try(value.asInstanceOf[A]).toOption }
+}
+
+object Properties {
+  def apply(ps: (String, Any)*): Properties = Properties(Map(ps: _*))
+}
