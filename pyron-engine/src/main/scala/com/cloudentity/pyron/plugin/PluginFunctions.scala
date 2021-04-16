33cbe7598de50f3fa46505efb408d4e908f28210
Aleksander Swirski <aswirski@cloudentity.com>
2021-04-16 08:02:09 +0200

    Tidy PluginFunctions

--- pyron-engine/src/main/scala/com/cloudentity/pyron/plugin/PluginFunctions.scala
+++ pyron-engine/src/main/scala/com/cloudentity/pyron/plugin/PluginFunctions.scala
@@ -6,36 +6,32 @@ import com.cloudentity.pyron.domain.http.ApiResponse
 import scala.concurrent.{ExecutionContext, Future}
 
 object PluginFunctions {
-  type RequestPlugin  = RequestCtx => Future[RequestCtx] // NOTE: RequestPlugin may return failed Future
-  type ResponsePlugin = ResponseCtx => Future[ResponseCtx] // NOTE: ResponsePlugin may return failed Future
+  private type Plugin[Ctx] = Ctx => Future[Ctx]
+  type RequestPlugin  = Plugin[RequestCtx] // NOTE: RequestPlugin may return failed Future
+  type ResponsePlugin = Plugin[ResponseCtx] // NOTE: ResponsePlugin may return failed Future
 
   def applyRequestPlugins(requestCtx: RequestCtx, plugins: List[RequestPlugin])
                          (recoveredResponse: Throwable => ApiResponse)
-                         (implicit ec: ExecutionContext): Future[RequestCtx] = {
-    val initial = Future.successful(requestCtx)
-    plugins.foldLeft(initial) { (future, plugin) =>
-      future.flatMap { ctx =>
-        ctx.aborted match {
-          case Some(_) => Future.successful(ctx)
-          case None => plugin.apply(ctx).recover { case ex =>
-            ctx.copy(failed = Some(RequestPluginFailure)).abort(recoveredResponse(ex))
-          }
-        }
+                         (implicit ec: ExecutionContext): Future[RequestCtx] =
+    applyPlugins(requestCtx, plugins)((ctx, plugin) => ctx.aborted.fold {
+      plugin(ctx).recover { case ex =>
+        ctx.copy(failed = Some(RequestPluginFailure)).abort(recoveredResponse(ex))
       }
-    }
-  }
+    } { _ => Future.successful(ctx) })
 
   def applyResponsePlugins(responseCtx: ResponseCtx, plugins: List[ResponsePlugin])
                           (recoveredResponse: Throwable => ApiResponse)
-                          (implicit ec: ExecutionContext): Future[ResponseCtx] = {
-    val initial = Future.successful(responseCtx)
-    plugins.foldLeft(initial) { (future, plugin) =>
-      future.flatMap { ctx =>
-        plugin.apply(ctx).recover { case ex =>
-          ctx.copy(failed = Some(ResponsePluginFailure), response = recoveredResponse(ex))
-        }
-      }
-    }
+                          (implicit ec: ExecutionContext): Future[ResponseCtx] =
+    applyPlugins(responseCtx, plugins)((ctx, plugin) =>
+      plugin(ctx).recover { case ex =>
+        ctx.copy(failed = Some(ResponsePluginFailure), response = recoveredResponse(ex))
+      })
+
+  private def applyPlugins[Ctx](ctx: Ctx, plugins: List[Plugin[Ctx]])
+                               (applyPlugin: (Ctx, Plugin[Ctx]) => Future[Ctx])
+                               (implicit ec: ExecutionContext): Future[Ctx] = {
+    val initial = Future.successful(ctx)
+    plugins.foldLeft(initial) { (ctxFuture, plugin) => ctxFuture.flatMap(applyPlugin(_, plugin)) }
   }
 
 }
