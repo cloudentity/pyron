b41631e4280e33b8ec54f3b84582363cbfc66eab
Aleksander Swirski <aswirski@cloudentity.com>
2021-06-04 08:26:00 +0200

    Add changes from dev

---
+++ pyron-plugin-impl/src/test/scala/com/cloudentity/pyron/plugin/impl/transform/request/TransformRequestPluginDeploymentTest.scala
@@ -0,0 +1,44 @@
+package com.cloudentity.pyron.plugin.impl.transform.request
+
+import com.cloudentity.pyron.plugin.impl.PluginAcceptanceTest
+import io.restassured.RestAssured.given
+import org.junit.{After, Before, Test}
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.integration.ClientAndServer.startClientAndServer
+import org.mockserver.model.{HttpRequest, HttpResponse}
+import org.scalatest.MustMatchers
+
+class TransformRequestPluginDeploymentTest extends PluginAcceptanceTest with MustMatchers {
+  override def getMetaConfPath: String = "src/test/resources/plugins/transform-request/meta-config-deployment.json"
+
+  var targetService: ClientAndServer = _
+
+  @Before
+  def before(): Unit = {
+    targetService = startClientAndServer(7760)
+    targetService.when(new HttpRequest()).respond(new HttpResponse().withStatusCode(200))
+  }
+
+  @After
+  def finish(): Unit = {
+    targetService.stop
+  }
+
+  @Test
+  def shouldDeployPluginWithoutPLUGIN_TRANSFORM_REQUEST_CONF_REF(): Unit = {
+    given()
+      .when()
+      .get("/fixed-path-param/value")
+      .`then`()
+      .statusCode(200)
+
+    assertTargetRequest { req =>
+      req.getPath mustBe "/fixed-path-param/fixed-param"
+    }
+  }
+
+  def assertTargetRequest(f: HttpRequest => Unit): Unit = {
+    targetService.retrieveRecordedRequests(null).length mustBe 1
+    f(targetService.retrieveRecordedRequests(null)(0))
+  }
+}
