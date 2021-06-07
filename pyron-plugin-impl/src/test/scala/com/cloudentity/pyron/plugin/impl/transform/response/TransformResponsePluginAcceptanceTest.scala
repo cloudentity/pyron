c42b72e2b6ea2ef3a74780c780f740d1cdaec9a5
Aleksander Swirski <aswirski@cloudentity.com>
2021-06-07 10:31:55 +0200

    Add some tests for dynamic scopes to prove these also work fine

--- pyron-plugin-impl/src/test/scala/com/cloudentity/pyron/plugin/impl/transform/response/TransformResponsePluginAcceptanceTest.scala
+++ pyron-plugin-impl/src/test/scala/com/cloudentity/pyron/plugin/impl/transform/response/TransformResponsePluginAcceptanceTest.scala
@@ -10,21 +10,48 @@ import org.mockserver.model.{HttpRequest, HttpResponse}
 import org.scalatest.MustMatchers
 
 import scala.collection.JavaConverters._
+import scala.language.implicitConversions
+import scala.util.Random
 
 class TransformResponsePluginAcceptanceTest extends PluginAcceptanceTest with MustMatchers {
+
+  def asJavaArray[A](l: List[A]): java.util.ArrayList[A] = new java.util.ArrayList[A](l.asJava)
 
   var targetService: ClientAndServer = _
 
   override def getMetaConfPath: String = "src/test/resources/plugins/transform-response/meta-config.json"
 
+  val rand: Random.type = scala.util.Random
+  val transactionId: Int = rand.nextInt.abs
+  val paymentId = rand.nextInt.abs
+  val swiftId: Int = rand.nextInt.abs
+  val envIdNoSuffix: Int = rand.nextInt.abs
+  val envIdSuffixed: Int = rand.nextInt.abs
+  val idOne: Int = rand.nextInt.abs
+  val idTwo: Int = rand.nextInt.abs
+
   @Before
   def before(): Unit = {
+
     targetService = startClientAndServer(7760)
     targetService.when(new HttpRequest()).respond(new HttpResponse()
       .withHeader("Content-Type", "application/json")
-      .withBody("""{"h": "bodyParamValue"}""")
       .withHeader("userUuid", "12345")
-      .withStatusCode(200)
+      .withBody(
+        s"""{
+           |"h": "bodyParamValue",
+           |"groups": "admin",
+           |"scp": [
+           |  "id.$idOne",
+           |  "id.$idTwo",
+           |  "unrelated-value-123",
+           |  "transaction.$transactionId/swift.$swiftId",
+           |  "env.$envIdNoSuffix",
+           |  "env.$envIdSuffixed.suffix",
+           |  "non-payment.$paymentId"
+           |]
+           |}""".stripMargin
+      ).withStatusCode(200)
     )
   }
 
@@ -50,7 +77,7 @@ class TransformResponsePluginAcceptanceTest extends PluginAcceptanceTest with Mu
       .when()
       .get("/body-from-query-param")
       .`then`()
-      .body("attr", equalTo(new java.util.ArrayList(List("one", "two", "six").asJava)))
+      .body("attr", equalTo(asJavaArray(List("one", "two", "six"))))
       .statusCode(200)
   }
 
@@ -148,4 +175,32 @@ class TransformResponsePluginAcceptanceTest extends PluginAcceptanceTest with Mu
       .statusCode(200)
   }
 
+  @Test
+  def shouldSetHeaderFromDynamicScopeAndFindMultipleParamsInPatternAndReorderThemInOutput(): Unit = {
+    given()
+      .when()
+      .get("/dyn-header-can-find-multiple-params-in-pattern-and-reorder-them-in-input")
+      .`then`()
+      .header("X-Transaction", equalTo(s"$swiftId.$transactionId"))
+      .statusCode(200)
+  }
+
+  @Test
+  def shouldSetHeaderWhenPatternMatchesEntireValueButNotWhenItMatchesPartOfAWiderValue(): Unit = {
+    given()
+      .when()
+    .get("/dyn-header-requires-pattern-matching-on-entire-value")
+      .`then`()
+      .header("X-Transaction", equalTo(s"$transactionId/swift.$swiftId"))
+      .header("X-Payment", equalTo(null))
+      .header("X-Env", equalTo(s"$envIdSuffixed"))
+  }
+
+  @Test
+  def shouldSetHeaderFromDynamicScopeUsingAllOfTheValuesWhichMatchThePattern(): Unit = {
+    given()
+      .when()
+      .get("/dyn-header-will-obtain-all-the-values-matching-the-pattern")
+      .getHeaders.getValues("X-Id") mustBe asJavaArray(List(s"$idOne", s"$idTwo"))
+  }
 }
