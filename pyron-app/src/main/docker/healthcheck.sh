2a7689ad83f4a5ea1246269a1424d132c8629eec
Artur Smolarek <artursmolarek@gmail.com>
2021-11-18 14:34:23 +0100

    Add  variable

--- pyron-app/src/main/docker/healthcheck.sh
+++ pyron-app/src/main/docker/healthcheck.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-PORT=${PORT:-8080}
-curl -k -f ${HEALTHCHECK_URL:-http://localhost:$PORT/alive} || exit 1
+curl -k -f ${HEALTHCHECK_URL:-http://localhost:$HTTP_SERVER_PORT/alive} || exit 1
