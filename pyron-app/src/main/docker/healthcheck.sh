9b048651747629ff808dbabc742de61b3a2f3b40
Artur Smolarek <artursmolarek@gmail.com>
2021-11-18 16:20:51 +0100

    Dockerfile adjustments

--- pyron-app/src/main/docker/healthcheck.sh
+++ pyron-app/src/main/docker/healthcheck.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
 
+HTTP_SERVER_PORT=${HTTP_SERVER_PORT:-8080}
+export HTTP_SERVER_PORT=${PORT:-$HTTP_SERVER_PORT}
+
 curl -k -f ${HEALTHCHECK_URL:-http://localhost:$HTTP_SERVER_PORT/alive} || exit 1
